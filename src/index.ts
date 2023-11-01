4cc9195965661ad1f046a04caee065ab9e78e6c4
Saif Alqady <saifalqady@192.168.1.8>
2023-11-01 19:32:56 +0200

    changing get request logic

--- src/index.ts
+++ src/index.ts
@@ -1,75 +1,54 @@
 import express, { Request, Response, Application } from 'express';
 import helmet from 'helmet';
-import { citiesList } from './TopThousandCity';
-import {getAllCities} from './utils/getAllCities';
-import { getCityData } from './utils/getCityData';
-import getIATACityCode from './utils/getIATACityCode';
+import { getCityIATACode, getCityName } from './utils/getCityData';
 import { insertCity } from './utils/insertCity';
 import cors from 'cors';
 
-const listOfCities = citiesList.slice(900, 1000);
 const app: Application = express();
 app.use(helmet());
-app.use(express.json())
-app.use(cors({origin: '*'}))
+app.use(express.json());
+app.use(cors({ origin: '*' }));
 
 app.get('/', (_req: Request, res: Response) => {
     res.json({ message: 'hello world' });
 });
 
-let modifiedList:string[] = []
-
-app.get('/get/all', async(_req:Request,res:Response) => {
-	for(let city of listOfCities){
-		const existedcity = await getAllCities(city)
-		if(existedcity !== null){
-			modifiedList.push(existedcity?.name)
-			console.log(`adding city ${city} to modifiedList`)
-			console.log(modifiedList)
-		}
-		else{
-			console.log(`city ${city} does not exist in database`)
-
-		}
-	}
-	res.send('don')
-})
-
 // get city IATA code from database
 app.get('/get', async (req: Request, res: Response) => {
-    const { cityName } = req.query;
-    if (cityName) {
-        const city = await getCityData(cityName);
-        if (city) {
-            res.json(city);
+    const { cityName, IATACode } = req.query;
+    if (IATACode) {
+        const cityData = await getCityName(IATACode);
+        if (IATACode) {
+            res.json(cityData);
         }
-    } else {
+    }
+    if (cityName) {
+        const cityData = await getCityIATACode(cityName);
+        if (cityData) {
+            res.json(cityData);
+        }
+    } else if(!IATACode && !cityName) {
         res.json({
             message:
-                'make sure that your url is in the form of get/?cityName={cityname}',
+                'make sure that your url is in the form of get/?cityName={cityname} or get/?IATACode={IATA}',
         });
     }
+	res.end()
 });
 
-app.post('/upload', async (_req: Request, _res: Response) => {
-    for (let cityName of listOfCities) {
-        const cityCode = await getIATACityCode(cityName);
-        if (cityCode) {
-            await insertCity(cityName, cityCode);
-            console.log(`city ${cityName} inserted into database`);
-        } else {
-            console.log(`city code for ${cityName} not available`);
-        }
-    }
-});
 app.post('/upload/single', async (req: Request, res: Response) => {
-    const { cityName, cityCode } = req.query;
-    if (cityName && cityCode) {
-        await insertCity(cityName, cityCode);
+    const { cityName, cityCode, api_key } = req.query;
+    if (api_key !== process.env.API_KEY) {
+        if (cityName && cityCode) {
+            await insertCity(cityName, cityCode);
+        } else {
+            res.json({
+                message:
+                    'make sure that the url in this form => /upload/single/?cityName={}&cityCode={}',
+            });
+        }
     } else {
-        res.send(
-            'make sure that the url in this form => /upload/single/?cityName={}&cityCode={}',
-        );
+        res.json({ message: 'you are not allowed to upload without api_key' });
     }
 });
 
--- src/utils/getCityData.ts
+++ src/utils/getCityData.ts
@@ -1,6 +1,6 @@
 import { client, run, disconnect } from '../database/mongodb_connect';
 
-export async function getCityData(cityName: any) {
+export async function getCityIATACode(cityName: any) {
     try {
         // start mongodb connection
         await run();
@@ -11,6 +11,28 @@ export async function getCityData(cityName: any) {
             .findOne({ name: cityName });
         if (cityData == null) {
             return { message: `${cityName} not found` };
+        } else {
+            return { cityName: cityData.name, IATA: cityData.IATA };
+        }
+    } catch (error) {
+        console.error(error);
+    } finally {
+        // end mongodb connectiong
+        await disconnect();
+    }
+}
+
+export async function getCityName(IATACode: any) {
+    try {
+        // start mongodb connection
+        await run();
+
+        const cityData = await client
+            .db('test')
+            .collection('citycodes')
+            .findOne({ IATA: IATACode });
+        if (cityData == null) {
+            return { message: `${IATACode} not found` };
         } else {
             return { cityName: cityData.name, IATA: cityData.IATA };
         }
